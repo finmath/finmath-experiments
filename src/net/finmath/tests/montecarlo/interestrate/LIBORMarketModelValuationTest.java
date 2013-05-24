a819001c9bca80ba29fecd718b3c38e8f19be45e
Christian Fries <email@christian-fries.de>
2013-05-24 22:40:02 +0000

    Improved LMM unit test.
    git-svn-id: http://svn.finmath.net/finmath%20experiments/trunk@388 48fcbb9e-5639-0410-8b25-842128a4905d

--- src/net/finmath/tests/montecarlo/interestrate/LIBORMarketModelValuationTest.java
+++ src/net/finmath/tests/montecarlo/interestrate/LIBORMarketModelValuationTest.java
@@ -47,7 +47,7 @@ public class LIBORMarketModelValuationTest {
 
 	private boolean isUnitTests = true;
 	
-	private final int numberOfPaths		= 5000;
+	private final int numberOfPaths		= 10000;
 	private final int numberOfFactors	= 5;
 	
 	private LIBORModelMonteCarloSimulationInterface liborMarketModel; 
@@ -222,7 +222,7 @@ public class LIBORMarketModelValuationTest {
 			System.out.println(formatterDeviation.format(deviation));
 			
 			
-			if(isUnitTests) assertTrue(Math.abs(deviation) < 2E-03);
+			if(isUnitTests) assertTrue(Math.abs(deviation) < 8E-04);
 		}
 		System.out.println("__________________________________________________________________________________________\n");
 	}
@@ -274,7 +274,7 @@ public class LIBORMarketModelValuationTest {
 			System.out.print(formatterPrice.format(value) + "\n");
 
 			// The swap should be at par (close to zero)
-			if(isUnitTests) assertTrue(Math.abs(value) < 2E-3);
+			if(isUnitTests) assertTrue(Math.abs(value) < 1E-3);
 		}
 		System.out.println("__________________________________________________________________________________________\n");
 	}

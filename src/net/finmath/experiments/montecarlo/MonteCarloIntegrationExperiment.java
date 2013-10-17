b3fcc24d14646dc19716fa676c33793e0f6846c7
Christian Fries <email@christian-fries.de>
2013-10-17 20:31:16 +0000

    Added a demo for Monte-Carlo integration.
    git-svn-id: http://svn.finmath.net/finmath%20experiments/trunk@707 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/net/finmath/experiments/montecarlo/MonteCarloIntegrationExperiment.java
@@ -0,0 +1,49 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 12.10.2013
+ */
+
+package net.finmath.experiments.montecarlo;
+
+/**
+ * @author Christian Fries
+ */
+public class MonteCarloIntegrationExperiment {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		long numberOfSimulations = 1000000;
+		
+		double pi = getMonteCarloApproximationOfPi(numberOfSimulations);
+		System.out.println("Simulation with n=" + numberOfSimulations + " resulted in approximation of pi = " + pi);
+
+		System.out.println("Approximation error is                       = " + (pi-Math.PI));
+		System.out.println("Theoretical order of th Monte-Carlo error is = "+1.0/Math.sqrt(numberOfSimulations));
+	}
+
+	/**
+	 * Calculates an approximation of pi via Monte-Carlo integration.
+	 * 
+	 * @param numberOfSimulations
+	 * @return An approximation of pi.
+	 */
+	public static double getMonteCarloApproximationOfPi(long numberOfSimulations) {
+		long numberOfPointsInsideUnitCircle = 0;
+		for(long i=0; i<numberOfSimulations; i++) {
+			double x = 2.0 * (Math.random() - 0.5);
+			double y = 2.0 * (Math.random() - 0.5);
+			if(x*x + y*y < 1.0) numberOfPointsInsideUnitCircle++;
+		}
+		
+		double areaOfUnitCircle = 4.0 * (double)numberOfPointsInsideUnitCircle / (double)numberOfSimulations;
+		
+		// The theoretical area of a circle is pi r^2. Hence we have:
+		double pi = areaOfUnitCircle;
+
+		return pi;
+	}
+}

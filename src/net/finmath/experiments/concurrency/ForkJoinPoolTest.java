4e44093f2dad73a32fd67e6fac0b5abab3926468
Christian Fries <email@christian-fries.de>
2014-05-03 07:27:08 +0000

    Added a demo for a deadlock using parallel streams.
    git-svn-id: http://svn.finmath.net/finmath%20experiments/trunk@1040 48fcbb9e-5639-0410-8b25-842128a4905d

---
+++ src/net/finmath/experiments/concurrency/ForkJoinPoolTest.java
@@ -0,0 +1,30 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christianfries.com.
+ *
+ * Created on 02.05.2014
+ */
+
+package net.finmath.experiments.concurrency;
+
+/**
+ * @author Christian Fries
+ *
+ */
+public class ForkJoinPoolTest {
+
+	/**
+	 * 
+	 */
+	public ForkJoinPoolTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}

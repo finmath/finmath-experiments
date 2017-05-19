b884b504cef94af79a1bb7e51e0b30158c109ec5
Christian Fries <email@christian-fries.de>
2017-05-19 08:00:16 +0200

    Changed implementation to long (integer).

--- src/net/finmath/experiments/montecarlo/MonteCarloIntegrationWithQuasiRandomNumbersExperiment.java
+++ src/net/finmath/experiments/montecarlo/MonteCarloIntegrationWithQuasiRandomNumbersExperiment.java
@@ -22,14 +22,22 @@ public class MonteCarloIntegrationWithQuasiRandomNumbersExperiment {
 	 * @param args Arguments, not used
 	 */
 	public static void main(String[] args) {
-		int numberOfSimulations = 10000000;
-		
+		long numberOfSimulations = 10000000;
+
+		// Measure calculation time - start
+		long millisStart = System.currentTimeMillis();
 		
 		double pi = getMonteCarloApproximationOfPi(numberOfSimulations);
+
+		// Measure calculation time - end
+		long millisEnd = System.currentTimeMillis();
+
 		System.out.println("Simulation with n = " + numberOfSimulations + " resulted in approximation of pi = " + pi);
 
 		System.out.println("Approximation error is                        = " + Math.abs(pi-Math.PI));
 		System.out.println("Theoretical order of the Monte-Carlo error is = " + 1.0/Math.sqrt(numberOfSimulations));
+
+		System.out.println("Calculation took " + (millisEnd-millisStart)/1000.0 + " sec.");
 	}
 
 	/**
@@ -38,9 +46,9 @@ public class MonteCarloIntegrationWithQuasiRandomNumbersExperiment {
 	 * @param numberOfSimulations The number of elements to use from the random number sequence.
 	 * @return An approximation of pi.
 	 */
-	public static double getMonteCarloApproximationOfPi(int numberOfSimulations) {
+	public static double getMonteCarloApproximationOfPi(long numberOfSimulations) {
 		long numberOfPointsInsideUnitCircle = 0;
-		for(int i=0; i<numberOfSimulations; i++) {
+		for(long i=0; i<numberOfSimulations; i++) {
 			double x = 2.0 * (HaltonSequence.getHaltonNumber(i, 2) - 0.5);
 			double y = 2.0 * (HaltonSequence.getHaltonNumber(i, 3) - 0.5);
 			if(x*x + y*y < 1.0) numberOfPointsInsideUnitCircle++;

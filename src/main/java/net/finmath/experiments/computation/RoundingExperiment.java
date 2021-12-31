6a1cd72330aa4d1f46c803eb293a4002e1d086cb
Christian Fries <email@christian-fries.de>
2021-12-31 01:26:35 +0100

    Added simple experiment related to rounding.

---
+++ src/main/java/net/finmath/experiments/computation/RoundingExperiment.java
@@ -0,0 +1,21 @@
+package net.finmath.experiments.computation;
+
+public class RoundingExperiment {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Rounding: ");
+
+		double x = 1.0;
+		
+		double epsilon = 1E-16;
+		
+		double y = x + epsilon;	// 1 + 10^-16 = 1.0000000000000001
+		
+		System.out.println("x = " + x);
+		System.out.println("𝜖 = " + epsilon);		
+		System.out.println("y = x + 𝜖 = " + y);
+		
+		System.out.println("x == y is " + (x==y));
+	}
+}

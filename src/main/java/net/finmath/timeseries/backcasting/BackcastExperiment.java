89085b39063fda44561f5cbb6bfef868e8f4ce66
Christian Fries <email@christian-fries.de>
2022-09-05 21:45:32 +0200

    Added path reconstruction code.

---
+++ src/main/java/net/finmath/timeseries/backcasting/BackcastExperiment.java
@@ -0,0 +1,147 @@
+package net.finmath.timeseries.backcasting;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.CholeskyDecomposition;
+
+import net.finmath.montecarlo.BrownianMotion;
+import net.finmath.montecarlo.BrownianMotionFromMersenneRandomNumbers;
+import net.finmath.montecarlo.BrownianMotionView;
+import net.finmath.montecarlo.CorrelatedBrownianMotion;
+import net.finmath.plots.DoubleToRandomVariableFunction;
+import net.finmath.plots.Plot;
+import net.finmath.plots.Plot2D;
+import net.finmath.plots.PlotableFunction2D;
+import net.finmath.stochastic.RandomVariable;
+import net.finmath.stochastic.Scalar;
+import net.finmath.time.TimeDiscretization;
+import net.finmath.time.TimeDiscretizationFromArray;
+
+public class BackcastExperiment {
+
+	public static void main(String[] args) throws Exception {
+
+		/*
+		 * Build a correlated n-dimension Brownian motion.
+		 */
+
+		double correlationDecay = 0.5;
+
+		int dimension = 20;
+		int timeSteps = 1000;
+		double timeHorizon = 1.0;
+		
+		double[][] correlationCandidate = new double[dimension][dimension];
+		for(int i=0; i<dimension; i++) {
+			for(int j=0; j<dimension; j++) {
+				correlationCandidate[i][j] = Math.exp(- correlationDecay * Math.abs(i-j));
+			}			
+		}
+
+//		double[][] rootMatrix = LinearAlgebra.getCholeskyDecomposition(correlationCandidate);
+		double[][] rootMatrix = new CholeskyDecomposition(new Array2DRowRealMatrix(correlationCandidate)).getL().getData();
+
+		
+		TimeDiscretization td = new TimeDiscretizationFromArray(0.0, timeSteps, timeHorizon/timeSteps);
+		int numberOfPath = 100;
+		int seed = 3141;
+
+		/*
+		 * Build various BrownianMotion s based on correlation model that constitute a market
+		 * 
+		 * The 0-th component of brownianIncrementCorrelated is the path, which we will remove and reconstruct
+		 */
+		BrownianMotion brownianIncrementUncorelated = new BrownianMotionFromMersenneRandomNumbers(td, dimension+1, numberOfPath, seed);
+		BrownianMotion brownianIncrementCorrelated = new CorrelatedBrownianMotion(new BrownianMotionView(brownianIncrementUncorelated,(Integer[]) IntStream.range(0, dimension).mapToObj(Integer::valueOf).toArray(i -> new Integer[i])), rootMatrix);
+		BrownianMotion brownianIncrementForResidual = new BrownianMotionView(brownianIncrementUncorelated, new Integer[] { dimension });
+
+		int pathIndex = 0;
+
+		// All market processes (0 to n-1)
+		RandomVariable[][] bmCorrelated = new RandomVariable[td.getNumberOfTimes()][dimension];
+		for(int j=0; j<dimension; j++) {
+			bmCorrelated[0][j] = new Scalar(0.0);
+			for(int i=0; i<td.getNumberOfTimeSteps(); i++) {
+				bmCorrelated[i+1][j] = bmCorrelated[i][j].add(brownianIncrementCorrelated.getBrownianIncrement(i, j));
+			}
+		}
+
+		// Build the market (note: index 0 is missing, the market are all processes after 1 to n-1)
+		Function<Integer, DoubleToRandomVariableFunction> marketProcesses = index -> time -> {
+			return bmCorrelated[td.getTimeIndexNearestLessOrEqual(time)][index+1];	// Note the index+1 on the correlated BM
+		};
+
+		// An uncorrelated BM (used in reconstruction)
+		RandomVariable[] bmResidual = new RandomVariable[td.getNumberOfTimes()];
+		bmResidual[0] = new Scalar(0.0);
+		for(int i=0; i<td.getNumberOfTimeSteps(); i++) {
+			bmResidual[i+1] = bmResidual[i].add(brownianIncrementForResidual.getBrownianIncrement(i, 0));
+		}
+		DoubleToRandomVariableFunction bmResProcesses = time -> {
+			return bmResidual[td.getTimeIndexNearestLessOrEqual(time)];
+		};
+		
+		
+		/*
+		 * Reconstruction based on the n-1 market processes and one independent BM.
+		 */
+
+		CorrelationBasedBackcast correlationBasedBackcast = new CorrelationBasedBackcast();
+
+		DoubleToRandomVariableFunction reconstruction = correlationBasedBackcast.buildReconstruction(marketProcesses, correlationCandidate, bmResProcesses);
+
+		/*
+		 * Drift adjustment - may also consider BrownianBridge
+		 */
+		DoubleToRandomVariableFunction reconstructionDriftAdjusted = t -> reconstruction.apply(t).add(pathIndex).add(
+				bmCorrelated[td.getNumberOfTimes()-1][0]
+						.sub(reconstruction.apply(timeHorizon)).mult(t/timeHorizon)
+						);
+
+		/*
+		 * Plot original path
+		 */
+		DoubleUnaryOperator pathOriginal = t -> {
+			return bmCorrelated[td.getTimeIndexNearestLessOrEqual(t)][0].get(pathIndex);
+		};
+
+		/*
+		 * Plot reconstructed path
+		 */
+		DoubleUnaryOperator pathReconstructed = t -> {
+			try {
+				return reconstructionDriftAdjusted.apply(t).get(pathIndex);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return 0;
+			}
+		};
+		
+
+		/*
+		 * Value only needed for plotting
+		 */
+		double[] generatorVector = correlationBasedBackcast.getGeneratorVector(correlationCandidate);
+		double correlationValue = Math.sqrt(Arrays.stream(generatorVector).map(x -> x*x).sum());
+
+		Plot plot = new Plot2D(List.of(
+				new PlotableFunction2D(0.0, timeHorizon, 100, pathReconstructed),
+				new PlotableFunction2D(0.0, timeHorizon, 100, pathOriginal)
+				));
+		plot
+		.setTitle("Original (green) and Reconstructed (red) Path (market correlation = " + String.format("%5.2f", correlationValue)+ ")")
+		.setXAxisLabel("time")
+		.setYAxisLabel("value")
+		.show();
+		plot.saveAsPDF(new File("backcast-" + (int)(correlationValue*100) + ".pdf"), 800, 400);
+		plot.saveAsJPG(new File("backcast-" + (int)(correlationValue*100) + ".jpg"), 800, 400);
+	}
+
+}
---
+++ src/main/java/net/finmath/timeseries/backcasting/CorrelationBasedBackcast.java
@@ -0,0 +1,73 @@
+package net.finmath.timeseries.backcasting;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.CholeskyDecomposition;
+
+import net.finmath.plots.DoubleToRandomVariableFunction;
+import net.finmath.stochastic.RandomVariable;
+import net.finmath.stochastic.Scalar;
+
+/**
+ * Correlation based reconstruction of a path of a Brownian motion.
+ * 
+ * Assume that \( dX = F dW where F = { (1,0) , (g,L) } \). Assume that \( dX1 = dW1 \) cannot be observed.
+ * We have \( dX_2 = g dW_1 + L dW_2 \). Since \( g^{T} L = 0 \) we find that \( dW1 = g^{T} dX_{2} / g^{T} g \).
+ * 
+ * @author Christian Fries
+ */
+public class CorrelationBasedBackcast {
+	
+	/**
+	 * Calculate the projection vector \( g \) from the correlation matrix.
+	 * 
+	 * @param correlation A symmetric positive definite \( n \times n \) matrix.
+	 * @return An n-1 vector.
+	 */
+	double[] getGeneratorVector(double[][] correlation) {
+		/*
+		 * From correlation extract factor matrix
+		 */
+//		double[][] rootMatrix = LinearAlgebra.getCholeskyDecomposition(correlation);
+		double[][] rootMatrix = new CholeskyDecomposition(new Array2DRowRealMatrix(correlation)).getL().getData();
+
+		int dimension = rootMatrix.length;
+		
+		// Take first column without first element: projection vector.
+		double[] generatorVector = new double[dimension-1];
+		for(int i=1; i<dimension; i++) {
+			for(int j=1; i<dimension; i++) {
+				generatorVector[j-1] += rootMatrix[i][0] / (dimension-1);
+			}
+		}
+		
+		return generatorVector;
+	}
+
+	/**
+	 * Build a reconstruction of the process X_1 using the processes X_2,\ldots,X_n, the correlation matrix and an independent Brownian motion W.
+	 * 
+	 * @param marketProcesses The market process i -> t -> X_{i+1}(t)
+	 * @param correlation The correlation matrix.
+	 * @param brownianForResidual The Brownian motion t -> W(t)
+	 * @return The process t -> X_{1}(t)
+	 */
+	DoubleToRandomVariableFunction buildReconstruction(Function<Integer, DoubleToRandomVariableFunction> marketProcesses, double[][] correlation, DoubleToRandomVariableFunction brownianForResidual) {
+		
+		double[] generatorVector = getGeneratorVector(correlation);
+		double generatorNormSquared = Arrays.stream(generatorVector).map(x -> x*x).sum();
+		
+		DoubleToRandomVariableFunction reconstruction = time -> {
+			RandomVariable value = new Scalar(0.0);
+			for(int i=0; i<generatorVector.length; i++) {
+				value = value.add(marketProcesses.apply(i).apply(time).mult(generatorVector[i]));
+			}
+			value = value.add(brownianForResidual.apply(time).mult(Math.sqrt(1-generatorNormSquared)));
+			return value;
+		};
+		
+		return reconstruction;
+	}
+}

5d69aa7963eb2ce4d4032fbef97df67ff4697f02
Christian Fries <email@christian-fries.de>
2022-12-22 13:38:16 +0100

    Added experiment on Monte-Carlo error of zero bond error.

--- src/main/java/net/finmath/experiments/montecarlo/interestrates/TermStructureMonteCarloSimulationExperiments.java
+++ src/main/java/net/finmath/experiments/montecarlo/interestrates/TermStructureMonteCarloSimulationExperiments.java
@@ -4,6 +4,7 @@
 package net.finmath.experiments.montecarlo.interestrates;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,69 +41,132 @@ public class TermStructureMonteCarloSimulationExperiments {
 	private static final int	seed			= 3141;
 
 	public static void main(String[] args) throws Exception {
-		//		(new TermStructureMonteCarloSimulationExperiments()).testBondUnderMeasure();
-		//		(new TermStructureMonteCarloSimulationExperiments()).testForwardRateUnderMeasure();
-		//		(new TermStructureMonteCarloSimulationExperiments()).testCapletSmile();
-		//		(new TermStructureMonteCarloSimulationExperiments()).testCapletSmiles();
-				(new TermStructureMonteCarloSimulationExperiments()).testShortRate();
-		//		(new TermStructureMonteCarloSimulationExperiments()).testCapletATMImpliedVol();
-		//		(new TermStructureMonteCarloSimulationExperiments()).testCapletATMImpliedVolInterpolation();
-		//		(new TermStructureMonteCarloSimulationExperiments()).testCapletSmilesOnGPU();
+
+		TermStructureMonteCarloSimulationExperiments experiments = new TermStructureMonteCarloSimulationExperiments();
+
+		// The dynamic of the forward rate curve in a 1 factor model under Q
+//		experiments.plotForwardCurvesAtTime(0.0 /* observation time */, 1 /* number of factors */);
+//		experiments.plotForwardCurvesAtTime(1.0 /* observation time */, 1 /* number of factors */);
+//		experiments.plotForwardCurvesAtTime(5.0 /* observation time */, 1 /* number of factors */);
+
+		// Approximation error of the zero coupon bond, dependency on the measure
+//		experiments.testBondUnderMeasure(0.5 /* simulationTimeStep */, 10000 /* numberOfPaths */, false /* useDiscountCurve */);
+//		experiments.testBondUnderMeasure(0.5 /* simulationTimeStep */, 50000 /* numberOfPaths */, false /* useDiscountCurve */);
+//		experiments.testBondUnderMeasure(0.5 /* simulationTimeStep */, 250000 /* numberOfPaths */, false /* useDiscountCurve */);
+//		experiments.testBondUnderMeasure(0.1 /* simulationTimeStep */, 50000 /* numberOfPaths */, false /* useDiscountCurve */);
+
+		// Approximation error of the zero coupon bond, using a discount curve as a control variate
+//		experiments.testBondUnderMeasure(0.5 /* simulationTimeStep */, 50000 /* numberOfPaths */, true /* useDiscountCurve */);
+		
+		// Approximation error of the forward rates
+//		experiments.testForwardRateUnderMeasure();
+
+		//
+//		experiments.testCapletSmile();
+//		experiments.testCapletSmiles();
+//		experiments.testShortRate();
+//		experiments.testCapletATMImpliedVol();
+//		experiments.testCapletATMImpliedVolInterpolation();
+//		experiments.testCapletSmilesOnGPU();
 	}
 
 	public TermStructureMonteCarloSimulationExperiments() throws CalculationException {}
 
-	public void testBondUnderMeasure() throws Exception {
+	private void plotForwardCurvesAtTime(double time, int numberOfFactors) throws CalculationException, IOException {
 		final RandomVariableFactory randomVariableFactory = new RandomVariableFromArrayFactory();
 
 		final double forwardRate = 0.05;				// constant forward rate
 		final double periodLength = 0.5;				// semi-annual tenor discretization
-		final double volatility = 0.30;					// constant volatility
+		final double timeHorizon = 20.0;				// last maturity in the simulated forward curve
+		final boolean useDiscountCurve = false;
+		final double volatility = 0.20;					// constant volatility
 		final double localVolNormalityBlend = 0.0;		// Lognormal model
+		final double correlationDecayParam = 0.03;		// one factor, correlation of all drivers is 1
+		final double simulationTimeStep = periodLength;
+		final String measure = "terminal";
+		final int	 numberOfPaths	= 50000;
+
+		final TermStructureMonteCarloSimulationModel simulationModel = ModelFactory.createTermStuctureModel(
+				randomVariableFactory,
+				measure,
+				forwardRate,
+				periodLength,
+				timeHorizon,
+				useDiscountCurve,
+				volatility,
+				localVolNormalityBlend,
+				correlationDecayParam,
+				simulationTimeStep,
+				numberOfFactors,
+				numberOfPaths, seed);
+	
+		DoubleToRandomVariableFunction curves = periodStart -> simulationModel.getForwardRate(time, periodStart, periodStart+periodLength);
+		PlotProcess2D plot = new PlotProcess2D(new TimeDiscretizationFromArray(time, 20, periodLength), curves, 100);
+		plot.setTitle("Forward curves T ⟼ L(T,T+∆T;t) (t = " + time + ", factors: " + numberOfFactors + ", measure: " + measure + ")").setXAxisLabel("period start T").setYAxisLabel("forward rate L").setYAxisNumberFormat(new DecimalFormat("#.##%")).show();
+		plot.show();
+
+		final String filename = "Curves-at-time-" + time + "-factors-" + numberOfFactors + "-measure-" + measure;
+		plot.saveAsPDF(new File(filename + ".pdf"), 900, 400);
+		plot.saveAsPNG(new File(filename + ".png"), 900, 400);
+	}
+
+	public void testBondUnderMeasure(double simulationTimeStep, int numberOfPaths, boolean useDiscountCurve) throws Exception {
+		final RandomVariableFactory randomVariableFactory = new RandomVariableFromArrayFactory();
+
+		final double forwardRate = 0.05;				// constant forward rate
+		final double periodLength = 0.5;				// semi-annual tenor discretization
+		final double timeHorizon = 20.0;				// last maturity in the simulated forward curve
+		final double volatility = 0.30;					// constant volatility
+		final double localVolNormalityBlend = 0.0;		// 0: Lognormal model, 1: normal model
 		final double correlationDecayParam = 0.0;		// one factor, correlation of all drivers is 1
 
-		for(final Boolean useDiscountCurve : new Boolean[] { false, true }) {
+		for(final String measure : new String[] { "terminal", "spot"}) {
+			
+			final TermStructureMonteCarloSimulationModel simulationModel = ModelFactory.createTermStuctureModel(
+					randomVariableFactory,
+					measure,
+					forwardRate,
+					periodLength,
+					timeHorizon,
+					useDiscountCurve,
+					volatility,
+					localVolNormalityBlend,
+					correlationDecayParam,
+					simulationTimeStep,
+					numberOfFactors,
+					numberOfPaths, seed);
 
-			for(final String measure : new String[] { "terminal", "spot"}) {
+			final List<Double> maturities = new ArrayList<Double>();
+			final List<Double> errors = new ArrayList<Double>();
+
+			System.out.println(String.format("\nZero Bond Approximation Errors (measure=%s, discount curve control=%s)", measure, useDiscountCurve));
+			System.out.println(String.format("%15s \t %15s \t %25s", "maturity", "value", "error"));
+			System.out.println("_".repeat(79));
+			for(double maturity = 0.5; maturity <= 20; maturity += 0.5) {
+				final TermStructureMonteCarloProduct product = new Bond(maturity);
+				final double value = product.getValue(simulationModel);
+				final double yieldMonteCarlo = -Math.log(value)/maturity;
+
+				final double valueAnalytic = 1.0/Math.pow((1+forwardRate*periodLength), maturity/periodLength);
+				final double yieldAnalytic = -Math.log(valueAnalytic)/maturity;
+//				final double error = value-valueAnalytic;
+				final double error = yieldMonteCarlo-yieldAnalytic;
 				
-				final TermStructureMonteCarloSimulationModel simulationModel = ModelFactory.createTermStuctureModel(
-						randomVariableFactory,
-						measure,
-						forwardRate,
-						periodLength,
-						useDiscountCurve,
-						volatility,
-						localVolNormalityBlend,
-						correlationDecayParam,
-						numberOfFactors,
-						numberOfPaths, seed);
+				maturities.add(maturity);
+				errors.add(error);
 
-				final List<Double> maturities = new ArrayList<Double>();
-				final List<Double> errors = new ArrayList<Double>();
-
-				for(double maturity = 0.5; maturity < 20; maturity += 0.5) {
-					final TermStructureMonteCarloProduct product = new Bond(maturity);
-					final double value = product.getValue(simulationModel);
-					final double yieldMonteCarlo = -Math.log(value)/maturity;
-
-					final double valueAnalytic = 1.0/Math.pow((1+forwardRate*periodLength), maturity/periodLength);
-					final double yieldAnalytic = -Math.log(valueAnalytic)/maturity;
-
-					maturities.add(maturity);
-					errors.add(yieldMonteCarlo-yieldAnalytic);
-				}
-
-				final Plot plot = Plots.createScatter(maturities, errors, 0.0, 0.2, 5)
-						.setTitle("Zero bond error when using " + measure + " measure" + (useDiscountCurve ? " and numeraire control variate." : "."))
-						.setXAxisLabel("maturity")
-						.setYAxisLabel("error")
-						.setYAxisNumberFormat(new DecimalFormat("0.0E00"));
-
-				final String filename = "BondDiscretizationError-measure-" + measure + (useDiscountCurve ? "-with-control" : "");
-				plot.saveAsSVG(new File(filename + ".svg"), 900, 400);
-				plot.saveAsPDF(new File(filename + ".pdf"), 900, 400);
-				plot.show();
+				System.out.println(String.format("%15s \t %15s \t %25s", String.format("%5.2f", maturity), String.format("%7.4f", value), String.format("%20.16e", error)));
 			}
+
+			final Plot plot = Plots.createScatter(maturities, errors, 0.0, 0.2, 5)
+					.setTitle("Zero bond error when using " + measure + " measure" + (useDiscountCurve ? " and numeraire control variate" : "" + " (Δt="+simulationTimeStep +",n=" + numberOfPaths +")"))
+					.setXAxisLabel("maturity")
+					.setYAxisLabel("error")
+					.setYAxisNumberFormat(new DecimalFormat("0.0E00"));
+
+			final String filename = "BondDiscretizationError-measure-" + measure + (useDiscountCurve ? "-with-control" : "" + "-dt=" + Double.valueOf(simulationTimeStep).toString().replace('.', '_')  + "-numberOfPaths=" + numberOfPaths);
+			plot.saveAsPDF(new File(filename + ".pdf"), 900, 400);
+			plot.show();
 		}
 	}
 
@@ -111,41 +175,51 @@ public class TermStructureMonteCarloSimulationExperiments {
 
 		final double forwardRate = 0.05;				// constant forward rate
 		final double periodLength = 0.5;				// semi-annual tenor discretization
+		final double timeHorizon = 20.0;				// last maturity in the simulated forward curve
 		final double volatility = 0.30;					// constant volatility
 		final double localVolNormalityBlend = 0.0;		// 0 = lognormal model and 1 = normal model
 		final double correlationDecayParam = 0.0;		// one factor, correlation of all drivers is 1
+		final double simulationTimeStep = periodLength;
 
 		for(final String measure : new String[] { "terminal", "spot"}) {
 						
 			for(final Boolean useDiscountCurve : new Boolean[] { false, true }) {
-			
+
 				final TermStructureMonteCarloSimulationModel lmm = ModelFactory.createTermStuctureModel(
 						randomVariableFactory,
 						measure,
 						forwardRate,
 						periodLength,
+						timeHorizon,
 						useDiscountCurve,
 						volatility,
 						localVolNormalityBlend,
 						correlationDecayParam,
+						simulationTimeStep,
 						numberOfFactors,
 						numberOfPaths, seed);
 
 				final List<Double> fixings = new ArrayList<Double>();
 				final List<Double> errors = new ArrayList<Double>();
 
+				System.out.println(String.format("\nForward Rates Approximation Errors (measure=%s, discount curve control=%s)", measure, useDiscountCurve));
+				System.out.println(String.format("%15s \t %15s \t %25s", "fixing", "value", "error"));
+				System.out.println("_".repeat(79));
 				for(double fixing = 0.5; fixing < 20; fixing += 0.5) {
 					final TermStructureMonteCarloProduct productForwardRate = new ForwardRate(fixing, fixing, fixing+periodLength, fixing+periodLength, periodLength);
 					final TermStructureMonteCarloProduct productBond = new Bond(fixing+periodLength);
 
 					final double valueBondAnalytic = 1.0/Math.pow((1+forwardRate*periodLength), (fixing+periodLength)/periodLength);
-					final double value = productForwardRate.getValue(lmm) / valueBondAnalytic;
-//					final double value = productForwardRate.getValue(lmm) / productBond.getValue(lmm);
+//					final double value = productForwardRate.getValue(lmm) / valueBondAnalytic;
+					final double value = productForwardRate.getValue(lmm) / productBond.getValue(lmm);
 
 					final double valueAnalytic = forwardRate * periodLength;
+					
+					final double error = value-valueAnalytic;
 
 					fixings.add(fixing);
 					errors.add(value-valueAnalytic);
+					System.out.println(String.format("%15s \t %15s \t %25s", String.format("%5.2f", fixing), String.format("%7.4f", value), String.format("%20.16e", error)));
 				}
 
 				final Plot plot = Plots.createScatter(fixings, errors, 0.0, 0.2, 5)
@@ -170,10 +244,12 @@ public class TermStructureMonteCarloSimulationExperiments {
 		final String simulationTimeInterpolationMethod = "round_down";
 		final double forwardRate = 0.05;
 		final double periodLength = 0.5;
+		final double timeHorizon = 20.0;				// last maturity in the simulated forward curve
 		final boolean useDiscountCurve = false;
 		final double volatility = 0.30;					// constant volatility
 		final double localVolNormalityBlend = 0.0;		// 0 = lognormal model and 1 = normal model
 		final double correlationDecayParam = 0.0;		// one factor, correlation of all drivers is 1
+		final double simulationTimeStep = periodLength;
 
 		for(final String measure : new String[] { "terminal", "spot"}) {
 			final TermStructureMonteCarloSimulationModel lmm = ModelFactory.createTermStuctureModel(
@@ -182,10 +258,12 @@ public class TermStructureMonteCarloSimulationExperiments {
 					simulationTimeInterpolationMethod,
 					forwardRate,
 					periodLength,
+					timeHorizon,
 					useDiscountCurve,
 					volatility,
 					localVolNormalityBlend,
 					correlationDecayParam,
+					simulationTimeStep,
 					numberOfFactors,
 					numberOfPaths, seed);
 
@@ -245,9 +323,11 @@ public class TermStructureMonteCarloSimulationExperiments {
 		final String measure = "spot";
 		final double forwardRate = 0.05;
 		final double periodLength = 0.5;
+		final double timeHorizon = 20.0;				// last maturity in the simulated forward curve
 		final boolean useDiscountCurve = false;
 		final double volatility = 0.30;					// constant volatility
 		final double correlationDecayParam = 0.0;		// one factor, correlation of all drivers is 1
+		final double simulationTimeStep = periodLength;
 
 		final List<Double> strikes = new ArrayList<Double>();
 		final Map<String, List<Double>> impliedVolCurves = new HashMap<>();
@@ -255,8 +335,8 @@ public class TermStructureMonteCarloSimulationExperiments {
 			
 			final TermStructureMonteCarloSimulationModel lmm = ModelFactory.createTermStuctureModel(
 					randomVariableFactory, measure, simulationTimeInterpolationMethod,
-					forwardRate, periodLength, useDiscountCurve, volatility, normality, correlationDecayParam,
-					numberOfFactors, numberOfPaths, seed);
+					forwardRate, periodLength, timeHorizon, useDiscountCurve, volatility, normality, correlationDecayParam,
+					simulationTimeStep, numberOfFactors, numberOfPaths, seed);
 
 			/*
 			 * Value different products
@@ -309,9 +389,11 @@ public class TermStructureMonteCarloSimulationExperiments {
 
 		final double forwardRate = 0.05;				// constant forward rate
 		final double periodLength = 0.5;				// semi-annual tenor discretization
+		final double timeHorizon = 20.0;				// last maturity in the simulated forward curve
 		final double volatility = 0.20;					// constant volatility
 		final double localVolNormalityBlend = 0.0;		// Lognormal model
 		final double correlationDecayParam = 0.0;		// one factor, correlation of all drivers is 1
+		final double simulationTimeStep = periodLength;
 		final Boolean useDiscountCurve = false; 
 
 		for(final String measure : new String[] { "spot"}) {
@@ -323,12 +405,13 @@ public class TermStructureMonteCarloSimulationExperiments {
 						measure,
 						forwardRate,
 						periodLength,
+						timeHorizon,
 						useDiscountCurve,
 						volatility,
 						volatilityExponentialDecay,
 						localVolNormalityBlend,
 						correlationDecayParam,
-						numberOfFactors,
+						simulationTimeStep, numberOfFactors,
 						numberOfPaths, seed);
 
 				DoubleToRandomVariableFunction shortRateProcess = time -> forwardRateModel.getForwardRate(time, time, time+periodLength);
@@ -356,6 +439,8 @@ public class TermStructureMonteCarloSimulationExperiments {
 		final String simulationTimeInterpolationMethod = "round_down";
 		final double forwardRate = 0.05;
 		final double periodLength = 0.5;
+		final double timeHorizon = 20.0;				// last maturity in the simulated forward curve
+		final double simulationTimeStep = periodLength;
 		final boolean useDiscountCurve = false;
 
 		final double volatility = 0.30;
@@ -366,9 +451,10 @@ public class TermStructureMonteCarloSimulationExperiments {
 				simulationTimeInterpolationMethod,
 				forwardRate,
 				periodLength,
+				timeHorizon,
 				useDiscountCurve,
 				volatility, 0.0, 0.0,
-				numberOfFactors,
+				simulationTimeStep, numberOfFactors,
 				numberOfPaths, seed);
 
 		final List<Double> maturities = new ArrayList<Double>();
@@ -402,7 +488,7 @@ public class TermStructureMonteCarloSimulationExperiments {
 		plot.show();
 
 		final String filename = "Caplet-Impliled-Vol" + measure + (useDiscountCurve ? "-with-control" : "");
-		plot.saveAsSVG(new File(filename + ".svg"), 900, 400);
+		plot.saveAsPDF(new File(filename + ".pdf"), 900, 400);
 	}
 
 	public void testCapletATMImpliedVolInterpolation() throws Exception {
@@ -412,6 +498,8 @@ public class TermStructureMonteCarloSimulationExperiments {
 		final String measure = "spot";
 		final double forwardRate = 0.05;
 		final double periodLength = 0.5;
+		final double timeHorizon = 20.0;				// last maturity in the simulated forward curve
+		final double simulationTimeStep = periodLength;
 		final boolean useDiscountCurve = false;
 
 		for(final String simulationTimeInterpolationMethod : new String[] { "round_down", "round_nearest" }) {
@@ -421,9 +509,10 @@ public class TermStructureMonteCarloSimulationExperiments {
 					simulationTimeInterpolationMethod,
 					forwardRate,
 					periodLength,
+					timeHorizon,
 					useDiscountCurve,
 					0.30, 0.0, 0.0,
-					numberOfFactors,
+					simulationTimeStep, numberOfFactors,
 					numberOfPaths, seed);
 
 			final List<Double> maturities = new ArrayList<Double>();
@@ -457,17 +546,19 @@ public class TermStructureMonteCarloSimulationExperiments {
 			plot.show();
 
 			final String filename = "Caplet-Impliled-Vol-" + simulationTimeInterpolationMethod;
-			plot.saveAsSVG(new File(filename + ".svg"), 900, 400);
+			plot.saveAsPDF(new File(filename + ".pdf"), 900, 400);
 		}
 	}
 
 	public void testCapletSmilesOnGPU() throws CalculationException {
 
-		final String simulationTimeInterpolationMethod = "round_down";
-		final String measure = "spot";
-		final double forwardRate = 0.05;
-		final double periodLength = 0.5;
-		final boolean useDiscountCurve = false;
+		final String	simulationTimeInterpolationMethod = "round_down";
+		final String	measure = "spot";
+		final double	forwardRate = 0.05;
+		final double	periodLength = 0.5;
+		final double	timeHorizon = 20.0;				// last maturity in the simulated forward curve
+		final boolean	useDiscountCurve = false;
+		final double simulationTimeStep = periodLength;
 		final int		numberOfPaths	= 100000;
 
 		for(final RandomVariableFactory randomVariableFactory : List.of(
@@ -485,9 +576,10 @@ public class TermStructureMonteCarloSimulationExperiments {
 						simulationTimeInterpolationMethod,
 						forwardRate,
 						periodLength,
+						timeHorizon,
 						useDiscountCurve,
 						0.30, normality, 0.0,
-						numberOfFactors,
+						simulationTimeStep, numberOfFactors,
 						numberOfPaths, seed);
 
 				final List<Double> impliedVolatilities = new ArrayList<Double>();

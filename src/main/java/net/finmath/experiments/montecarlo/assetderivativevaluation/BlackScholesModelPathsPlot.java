4be9ba40b0308b29075d87871a02013d8f2610a9
Christian Fries <email@christian-fries.de>
2023-04-26 20:37:05 +0200

    Fixed typo.

--- src/main/java/net/finmath/experiments/montecarlo/assetderivativevaluation/BlackScholesModelPathsPlot.java
+++ src/main/java/net/finmath/experiments/montecarlo/assetderivativevaluation/BlackScholesModelPathsPlot.java
@@ -1,24 +1,38 @@
 package net.finmath.experiments.montecarlo.assetderivativevaluation;
 
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import net.finmath.montecarlo.BrownianMotionFromMersenneRandomNumbers;
 import net.finmath.montecarlo.assetderivativevaluation.models.BlackScholesModel;
 import net.finmath.montecarlo.process.EulerSchemeFromProcessModel;
 import net.finmath.plots.DoubleToRandomVariableFunction;
+import net.finmath.plots.GraphStyle;
+import net.finmath.plots.Named;
+import net.finmath.plots.Plot2D;
 import net.finmath.plots.PlotProcess2D;
+import net.finmath.plots.Plotable2D;
+import net.finmath.plots.PlotableFunction2D;
 import net.finmath.time.TimeDiscretizationFromArray;
 
-public class BlackSchoesModelPathsPlot {
+public class BlackScholesModelPathsPlot {
 
 	public static void main(String[] args) {
 		double modelInitialValue = 100.0;	// S(0)
-		double modelRiskFreeRate = 0.05; 	// r
-		double modelVolatility = 0.20;		// σ
+		double modelRiskFreeRate = 0.08; 	// r
+		double modelVolatility = 0.10;		// σ
 
 		// Create a model
 		var model = new BlackScholesModel(modelInitialValue, modelRiskFreeRate, modelVolatility);
 
 		// Create a corresponding MC process from the model
-		var td = new TimeDiscretizationFromArray(0.0, 300, 0.01);
+		var td = new TimeDiscretizationFromArray(0.0, 500, 0.01);
 		var brownianMotion = new BrownianMotionFromMersenneRandomNumbers(td, 1, 10000, 3231);
 		var process = new EulerSchemeFromProcessModel(model, brownianMotion);
 
@@ -27,7 +41,7 @@ public class BlackSchoesModelPathsPlot {
 
 		// Plot 100 of paths against the given time discretization.
 		var plot = new PlotProcess2D(td, paths, 100);
-		plot.setTitle("Black Scholes model paths").setXAxisLabel("time").setYAxisLabel("value");
+		plot.setTitle("Black Scholes model paths t -> S(t, \u03c9\u1D62)").setXAxisLabel("time").setYAxisLabel("value");
 		plot.show();
 	}
 }

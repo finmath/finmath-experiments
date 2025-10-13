d569f275bc196958fc441242b4756e621b5db01b
Christian Fries <email@christian-fries.de>
2025-10-13 13:56:01 +0200

    Fixed bug reporing bound property on progress bar.

--- src/main/java/net/finmath/experiments/ui/ExperimentsTree.java
+++ src/main/java/net/finmath/experiments/ui/ExperimentsTree.java
@@ -148,7 +148,7 @@ public class ExperimentsTree extends Application {
 	private Parent getInfo() {
 		VBox box = new VBox(
 				new Label("Collection of Parameter Experiments based on Models from finmath lib"),
-				new Label("Version 2025-10-09"),
+				new Label("Version 2025-10-12"),
 				new Label("Select a topic on the left; set the parameters or select calculate.")
 				);
 		box.setAlignment(Pos.CENTER);
